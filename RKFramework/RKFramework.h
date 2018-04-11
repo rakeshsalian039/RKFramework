cd3bae143f782acd2e1e8b7d83b46aa98c3e08e5
Rakesh <rakesh@infiniteit.biz>
2018-04-11 14:29:53 +0530

    Initial Commit

---
+++ RKFramework/RKFramework.h
@@ -0,0 +1,19 @@
+//
+//  RKFramework.h
+//  RKFramework
+//
+//  Created by Jigar Shah on 4/11/18.
+//  Copyright © 2018 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKFramework.
+FOUNDATION_EXPORT double RKFrameworkVersionNumber;
+
+//! Project version string for RKFramework.
+FOUNDATION_EXPORT const unsigned char RKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKFramework/PublicHeader.h>
+
+
